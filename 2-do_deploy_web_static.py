63024d301a49c087147e4a88d9cc2ab04ef2e34e
Calebbkillom <Calebbkillom@gmail.com>
2023-09-15 12:19:15 +0000

    task_2_t&e

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,36 +19,44 @@ env.key_filename = "~/.ssh/school"
 
 def do_deploy(archive_path):
     """Function that distributes the archive to web servers"""
-
+    # Check if the archive file exists
     if not os.path.exists(archive_path):
         return False
 
-    # Getting the base name of the archive without the file extension
-    archive_name = os.path.splitext(os.path.basename(archive_path))[0]
+    try:
+        # Getting the base name of the archive without the file extension
+        archive_name = os.path.splitext(os.path.basename(archive_path))[0]
 
-    # Uploading the archive to the remote server's /tmp/ directory
-    put(archive_path, "/tmp/{}.tgz".format(archive_name))
+        # Upload the archive to the remote server's /tmp/ directory
+        put(archive_path, "/tmp/{}.tgz".format(archive_name))
 
-    # Extracting the archive to the desired folder
-    release_path = "/data/web_static/releases/{}/".format(archive_name)
-    run("mkdir -p {}".format(release_path))
-    run("tar -xzf /tmp/{}.tgz -C {}".format(archive_name, release_path))
+        # Extract the archive to the desired folder
+        release_path = "/data/web_static/releases/{}/".format(archive_name)
+        run("mkdir -p {}".format(release_path))
+        run("tar -xzf /tmp/{}.tgz -C {}".format(archive_name, release_path))
 
-    # Deleting the archive from the server
-    run("rm /tmp/{}.tgz".format(archive_name))
+        # Delete the archive from the server
+        run("rm /tmp/{}.tgz".format(archive_name))
 
-    # Move files from 'web_static' subdirectory to release folder
-    run("mv {}/web_static/* {}/"
-        .format(release_path.rstrip('/'), release_path.rstrip('/')))
+        # Move files from 'web_static' subdirectory to release folder
+        run("mv {}/web_static/* {}/"
+            .format(release_path.rstrip('/'), release_path.rstrip('/')))
 
-    # Removing the 'web_static' subdirectory
-    run("rm -rf {}/web_static".format(release_path))
+        # Remove the 'web_static' subdirectory
+        run("rm -rf {}/web_static".format(release_path))
 
-    # Removing the old symbolic link
-    run("rm -rf /data/web_static/current")
+        # Remove the old symbolic link
+        run("rm -rf /data/web_static/current")
 
-    # Creating a new symbolic link
-    run("ln -s {} /data/web_static/current".format(release_path))
+        # Create a new symbolic link
+        run("ln -s {} /data/web_static/current".format(release_path))
 
-    # Printing the "New version deployed!" message
-    print("New version deployed!")
+        # Print the "New version deployed!" message
+        print("New version deployed!")
+
+        # Return True on successful deployment
+        return True
+    except Exception as e:
+        # Handle any exceptions and return False
+        print("Error: {}".format(e))
+        return False
